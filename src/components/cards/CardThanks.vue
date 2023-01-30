6a0c60bf2f8de77d6d5c867ba2756c08a9f93bc9
Jeff Rossi <jrossi@intouchgps.com>
2023-01-30 08:26:15 -0500

    add thanks indication on contact form

---
+++ src/components/cards/CardThanks.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="card thanks-card">
+		Thank You <b>{{ FormatName(name) }}</b
+		>, for your interest, I will try to respond within one business day.
+	</div>
+</template>
+
+<script>
+import { FormatName } from '../../lib'
+export default {
+	props: ['name'],
+	data: () => ({
+		FormatName,
+	}),
+}
+</script>
+
+<style>
+.card {
+	margin: 0.5em 1em 0.5em 1em;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,138 +1,148 @@
 <template>
-  <div class="contact">
-    <BreadCrumbs :trail="trail" />
-    <ContentRender :rows="rows" />
-    <div class="box">
-      <b-steps v-model="activeStep" :animated="true">
-        <b-step-item label="Name">
-          <FormName :name="contact.Name" />
-        </b-step-item>
-        <b-step-item label="Address">
-          <FormAddress :address="contact.Address" />
-        </b-step-item>
-        <b-step-item label="Contact Methods">
-          <FormContactMethod :contact="contact" />
-        </b-step-item>
-        <b-step-item label="Message">
-          <FormMessage :contact="contact" />
-        </b-step-item>
-        <b-step-item label="Send">
-          <ContactConfirm :contact="contact" @sendContact="sendContact" />
-        </b-step-item>
-      </b-steps>
-    </div>
-  </div>
+	<div class="contact">
+		<BreadCrumbs :trail="trail" />
+		<ContentRender :rows="rows" />
+		<div class="box">
+			<div v-if="sent">
+				<CardThanks :name="contact.Name" />
+			</div>
+			<div v-else>
+				<b-steps v-model="activeStep" :animated="true">
+					<b-step-item label="Name">
+						<FormName :name="contact.Name" />
+					</b-step-item>
+					<b-step-item label="Address">
+						<FormAddress :address="contact.Address" />
+					</b-step-item>
+					<b-step-item label="Contact Methods">
+						<FormContactMethod :contact="contact" />
+					</b-step-item>
+					<b-step-item label="Message">
+						<FormMessage :contact="contact" />
+					</b-step-item>
+					<b-step-item label="Send">
+						<ContactConfirm :contact="contact" @sendContact="sendContact" />
+					</b-step-item>
+				</b-steps>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import BreadCrumbs from '../components/layout/BreadCrumbs.vue';
-import { contentUrl, ContentUUID, RemoveBlanks } from '../lib';
-import FormName from '../components/forms/FormName.vue';
-import FormAddress from '../components/forms/FormAddress.vue';
-import FormContactMethod from '../components/forms/FormContactMethod.vue';
-import FormMessage from '../components/forms/FormMessage.vue';
-import ContentRender from '../components/ContentRender.vue';
-import { CREATE_CONTACT_MUTATION } from '../graphql/mutations';
-import ContactConfirm from '../components/ContactConfirm.vue';
+import BreadCrumbs from '../components/layout/BreadCrumbs.vue'
+import { contentUrl, ContentUUID, RemoveBlanks } from '../lib'
+import FormName from '../components/forms/FormName.vue'
+import FormAddress from '../components/forms/FormAddress.vue'
+import FormContactMethod from '../components/forms/FormContactMethod.vue'
+import FormMessage from '../components/forms/FormMessage.vue'
+import ContentRender from '../components/ContentRender.vue'
+import { CREATE_CONTACT_MUTATION } from '../graphql/mutations'
+import ContactConfirm from '../components/ContactConfirm.vue'
+import CardThanks from '../components/cards/CardThanks.vue'
 
 export default {
-  name: 'Contact',
-  components: {
-    BreadCrumbs,
-    FormName,
-    FormAddress,
-    FormContactMethod,
-    FormMessage,
-    ContentRender,
-    ContactConfirm
-  },
-  data: () => ({
-    trail: [{ text: 'Home', href: '/' },{ text: 'Contact' }],
-    page: null,
-    rows: [],
-    activeStep: 0,
-    contact: {
-      Name: {
-        Salutation: '',
-        First: '',
-        Middle: '',
-        Last: '',
-        Suffix: ''
-      },
-      Address: {
-        Address: '',
-        Suite: '',
-        City: '',
-        State: '',
-        Zip: ''
-      },
-      Email: '',
-      EmailType: 'Work',
-      Phone: '',
-      PhoneType: 'Work',
-      Preferred: 'Email',
-      Subject: '',
-      Message: ''
-    },
-  }),
-  methods: {
-    async loadContent() {
-      let url = `${contentUrl}/page/${ContentUUID.Contact}`;
-      let results = await fetch(url);
-      if (results.ok) {
-        this.page = await results.json();
-        console.log(this.page);
-      }
-      url = `${contentUrl}/page/${ContentUUID.Contact}/row`;
-      results = await fetch(url);
-      if (results.ok) {
-        this.rows = await results.json();
-        console.log(this.rows);
-      }
-    },
-    sendContact() {
-      const {
-        Name,
-        Address,
-        Email,
-        EmailType,
-        Phone,
-        PhoneType,
-        Preferred,
-        Subject,
-        Message
-      } = RemoveBlanks(this.contact)
-      this.$apollo.mutate({
-        mutation: CREATE_CONTACT_MUTATION,
-        variables: {
-          Name,
-          Address,
-          Email,
-          EmailType,
-          Phone,
-          PhoneType,
-          Preferred,
-          Subject,
-          Message
-        }
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(e => {
-        console.log(e);
-      }) 
-    }
-  },
-  created() {
-    this.loadContent()
-  }
+	name: 'Contact',
+	components: {
+		BreadCrumbs,
+		FormName,
+		FormAddress,
+		FormContactMethod,
+		FormMessage,
+		ContentRender,
+		ContactConfirm,
+		CardThanks,
+	},
+	data: () => ({
+		trail: [{ text: 'Home', href: '/' }, { text: 'Contact' }],
+		page: null,
+		sent: false,
+		rows: [],
+		activeStep: 0,
+		contact: {
+			Name: {
+				Salutation: '',
+				First: '',
+				Middle: '',
+				Last: '',
+				Suffix: '',
+			},
+			Address: {
+				Address: '',
+				Suite: '',
+				City: '',
+				State: '',
+				Zip: '',
+			},
+			Email: '',
+			EmailType: 'Work',
+			Phone: '',
+			PhoneType: 'Work',
+			Preferred: 'Email',
+			Subject: '',
+			Message: '',
+		},
+	}),
+	methods: {
+		async loadContent() {
+			let url = `${contentUrl}/page/${ContentUUID.Contact}`
+			let results = await fetch(url)
+			if (results.ok) {
+				this.page = await results.json()
+				console.log(this.page)
+			}
+			url = `${contentUrl}/page/${ContentUUID.Contact}/row`
+			results = await fetch(url)
+			if (results.ok) {
+				this.rows = await results.json()
+				console.log(this.rows)
+			}
+		},
+		sendContact() {
+			const {
+				Name,
+				Address,
+				Email,
+				EmailType,
+				Phone,
+				PhoneType,
+				Preferred,
+				Subject,
+				Message,
+			} = RemoveBlanks(this.contact)
+			this.$apollo
+				.mutate({
+					mutation: CREATE_CONTACT_MUTATION,
+					variables: {
+						Name,
+						Address,
+						Email,
+						EmailType,
+						Phone,
+						PhoneType,
+						Preferred,
+						Subject,
+						Message,
+					},
+				})
+				.then((data) => {
+					console.log(data)
+					this.sent = true
+				})
+				.catch((e) => {
+					console.log(e)
+				})
+		},
+	},
+	created() {
+		this.loadContent()
+	},
 }
 </script>
 
 <style>
 strong.send-label {
-  display: inline-block;
-  width: 10em;
+	display: inline-block;
+	width: 10em;
 }
 </style>
